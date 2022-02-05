c81f20e8d8f024fbcabdd2c57df9756e153ce7e0
BuildTools <unconfigured@null.spigotmc.org>
2022-02-06 03:30:14 +0900

    Google Oauth2
    ----------< 5.4

--- src/test/java/com/book/aws/AwsApplicationTests.java
+++ src/test/java/com/book/aws/AwsApplicationTests.java
@@ -1,9 +1,10 @@
 package com.book.aws;
 
+import com.book.aws.springboot.AwsApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AwsApplication.class)
 class AwsApplicationTests {
 
     @Test
