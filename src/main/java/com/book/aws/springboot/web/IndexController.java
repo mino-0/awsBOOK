33c0d37940341ce2c07709816e8d5ecf12311c89
BuildTools <unconfigured@null.spigotmc.org>
2022-02-06 15:40:38 +0900

    Google Oauth2
    ----------< 5.4

--- src/main/java/com/book/aws/springboot/web/IndexController.java
+++ src/main/java/com/book/aws/springboot/web/IndexController.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
     private final PostsService postsService;
 
-    @GetMapping("/")
+    @GetMapping("")
     public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
@@ -27,12 +27,12 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping("/posts/save")
+    @GetMapping("posts/save")
     public String postsSave() {
         return "posts-save";
     }
 
-    @GetMapping("/posts/update/{id}")
+    @GetMapping("posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("posts", dto);
