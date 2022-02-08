c96da32c2c172202ec9b6419960fe48cce687c86
BuildTools <unconfigured@null.spigotmc.org>
2022-02-08 23:08:28 +0900

    Google Oauth2
    ----------< 5.4

--- src/main/java/com/book/aws/springboot/AwsApplication.java
+++ src/main/java/com/book/aws/springboot/AwsApplication.java
@@ -8,6 +8,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AwsApplication {
 
     public static void main(String[] args) {
+
+
+
         SpringApplication.run(AwsApplication.class, args);
     }
 
