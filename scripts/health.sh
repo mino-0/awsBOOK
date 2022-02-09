034928695312642a0685b4f1a846e34193814435
BuildTools <unconfigured@null.spigotmc.org>
2022-02-09 15:31:16 +0900

    Google Oauth2
    ----------< 5.4

--- scripts/health.sh
+++ scripts/health.sh
@@ -14,11 +14,11 @@ sleep 10
 
 for RETRY_COUNT in {1..10}
 do
-  RESPONSE=$( curl -s http://localhost:${IDEL_PORT}/profile )
-  UP_COUNT=$( echo ${RESPONSE} | grep 'real' | wc -l )
+  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
+  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 #  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -1)
 
-  if [ $(UP_COUNT) -ge 1]
+  if [ ${UP_COUNT} -ge 1]
     then #$up_count >=1 ("real" 문자열이 있는지 검증)
       echo "> Health Check 성공"
       switch_proxy
