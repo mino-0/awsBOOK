5a776dd0b1b31c0964bb0f2c6d66447211bc2095
BuildTools <unconfigured@null.spigotmc.org>
2022-02-09 15:02:04 +0900

    Google Oauth2
    ----------< 5.4

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -6,7 +6,7 @@ function find_idle_profile()
 {
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if[ ${RESPONSE_CODE} - ge 400] #400보다 크면(에러코드 400이상)
+  if[ ${RESPONSE_CODE} - ge 400]   #400보다 크면(에러코드 400이상)
   then
     CURRENT_PROFILE = real2
   else
