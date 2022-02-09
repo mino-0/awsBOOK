6ebb40fe7d4e83012fad99d64ae0dada5b9cacda
BuildTools <unconfigured@null.spigotmc.org>
2022-02-09 15:43:07 +0900

    Google Oauth2
    ----------< 5.4

--- scripts/health.sh
+++ scripts/health.sh
@@ -15,10 +15,10 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
+  UPCOUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 #  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -1)
 
-  if [ ${UP_COUNT} -ge 1]
+  if [ ${UP_COUNT} -ge 1 ]
     then #$up_count >=1 ("real" 문자열이 있는지 검증)
       echo "> Health Check 성공"
       switch_proxy
@@ -28,7 +28,7 @@ do
       echo "> Health check : ${RESPONSE}"
    fi
 
-  if [ ${RETRY_COUNT} -eq 10]
+  if [ ${RETRY_COUNT} -eq 10 ]
     then
       echo ">Health Check 실패."
       echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
